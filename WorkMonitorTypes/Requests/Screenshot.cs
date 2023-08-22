8f2672e390ce2b0b3fe881ad97e32c9b550753e0
C-FamilyDeveloper <vladmakushevvlad@yandex.ru>
2023-08-22 15:09:23 +0300

    Добавьте файлы проекта.

---
+++ WorkMonitorTypes/Requests/Screenshot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkMonitorTypes.Requests
+{
+    public class Screenshot
+    {
+        public byte[] Image { get; set; }
+        public DateTime ScreenshotDateTime { get; set; }
+        public string ClientName { get; set; }
+    }
+}
---
+++ WorkMonitorTypes/Requests/WorkerInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkMonitorTypes.Requests
+{
+    public class WorkerInfo
+    {
+        public string Worker { get; set; }
+        public DateTime EventDateTime { get; set; }
+        public TimeSpan WorkTime { get; set; }
+        public TimeSpan IdleTime { get; set; }
+        public string Application { get; set; }
+        public string? Site { get; set; }
+    }
+}
